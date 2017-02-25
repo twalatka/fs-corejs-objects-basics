daca6102c570f47503d89662b84e83bbc3857e5a
twalatka <twalatka@gmail.com>
2017-02-25 13:53:05 -0700

    Constructors F150 & Prius Feb 25

--- docs/classes/constructors/index.js
+++ docs/classes/constructors/index.js
@@ -1,1 +1,25 @@
 // Code goes here...
+export class Vehicle {
+    constructor(engineType, numberOfDoors, make) {
+        this.make = make;
+        this.engineType = engineType;
+        this.numberOfDoors = numberOfDoors;
+    }
+}
+
+export class Truck extends Vehicle {
+    constructor(make) {
+        super('V8', 2, make);
+
+
+    }
+}
+
+export class Car extends Vehicle {
+    constructor(make) {
+        super('V4', 4, make);
+    }
+}
+
+export const F150 = new Truck('Ford');
+export const Prius = new Car('Toyota');
